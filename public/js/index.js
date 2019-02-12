c8d69ff232900914e60155f21ce1cf8bb4a3672f
Hyunsang Cho <hyunsang.cho@one-line.com>
2019-02-12 11:46:50 +0900

    second commit

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyABfLzDvN9IlFiopWodUEr1q9nPClVCd0A",
+    authDomain: "jambam-gbook.firebaseapp.com",
+    databaseURL: "https://jambam-gbook.firebaseio.com",
+    projectId: "jambam-gbook",
+    storageBucket: "jambam-gbook.appspot.com",
+    messagingSenderId: "458777170648"
+  };
+  firebase.initializeApp(config);
